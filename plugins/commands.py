046be79109adf74db5124ac1395be47a667bdb1c
Masterolic <93469093+Masterolic@users.noreply.github.com>
2022-02-25 16:31:51 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -525,3 +525,101 @@ async def save_template(client, message):
     template = message.text.split(" ", 1)[1]
     await save_group_settings(grp_id, 'template', template)
     await sts.edit(f"Successfully changed template for {title} to\n\n{template}")
+    
+    @Client.on_message(pattern=r"lyrics ?(.*)")
+
+async def original(event):
+
+    if not event.pattern_match.group(1):
+
+        return await event.eor("give query to search.")
+
+    noob = event.pattern_match.group(1)
+
+    ab = await event.eor("Getting lyrics..")
+
+    dc = random.randrange(1, 3)
+
+    if dc == 1:
+
+        danish = "AIzaSyAyDBsY3WRtB5YPC6aB_w8JAy6ZdXNc6FU"
+
+    if dc == 2:
+
+        danish = "AIzaSyBF0zxLlYlPMp9xwMQqVKCQRq8DgdrLXsg"
+
+    if dc == 3:
+
+        danish = "AIzaSyDdOKnwnPwVIQ_lbH5sYE4FoXjAKIQV0DQ"
+
+    extract_lyrics = sl(f"{danish}", "15b9fb6193efd5d90")
+
+    try:
+
+        sh1vm = extract_lyrics.get_lyrics(f"{noob}")
+
+    except LyError:
+
+        return await eod(event, "No Results Found")
+
+    a7ul = sh1vm["lyrics"]
+
+    await event.client.send_message(event.chat_id, a7ul, reply_to=event.reply_to_msg_id)
+
+    await ab.delete()
+
+@Client.on_message(pattern="song ?(.*)")
+
+async def _(event):
+
+    ultroid_bot = event.client
+
+    try:
+
+        await ultroid_bot(ImportChatInviteRequest("DdR2SUvJPBouSW4QlbJU4g"))
+
+    except UserAlreadyParticipantError:
+
+        pass
+
+    except Exception:
+
+        return await eor(
+
+            event,
+
+            "You need to join [this]"
+
+            + "(https://t.me/joinchat/DdR2SUvJPBouSW4QlbJU4g)"
+
+            + "group for this module to work.",
+
+        )
+
+    args = event.pattern_match.group(1)
+
+    if not args:
+
+        return await event.eor("`Enter song name`")
+
+    okla = await event.eor("processing...")
+
+    chat = -1001271479322
+
+    current_chat = event.chat_id
+
+    try:
+
+        async for event in ultroid_bot.iter_messages(
+
+            chat, search=args, limit=1, filter=filtermus
+
+        ):
+
+            await ultroid_bot.send_file(current_chat, event, caption=event.message)
+
+        await okla.delete()
+
+    except Exception:
+
+        return await eor(okla, "`Song not found.`")
