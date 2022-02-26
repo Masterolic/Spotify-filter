da928c0465f05b707c1da845d1e05ee65c77528d
Masterolic <93469093+Masterolic@users.noreply.github.com>
2022-02-26 11:39:05 +0530

    Update lyrics.py

--- plugins/lyrics.py
+++ plugins/lyrics.py
@@ -17,7 +17,22 @@
     alternative song command.
 """
 
-
+import os
+import logging
+import random
+import asyncio
+from Script import script
+from pyrogram import Client, filters
+from pyrogram.errors import ChatAdminRequired, FloodWait
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from database.ia_filterdb import Media, get_file_details, unpack_new_file_id
+from database.users_chats_db import db
+from info import CHANNELS, ADMINS, AUTH_CHANNEL, LOG_CHANNEL, PICS, BATCH_FILE_CAPTION, CUSTOM_FILE_CAPTION, PROTECT_CONTENT
+from utils import get_settings, get_size, is_subscribed, save_group_settings, temp
+from database.connections_mdb import active_connection
+import re
+import json
+import base64
 import random
 from pyrogram import client
 from lyrics_extractor import SongLyrics as sl
