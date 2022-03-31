da255aefb7e359861b1d7499dd1e9abc538b8800
Masterolic <93469093+Masterolic@users.noreply.github.com>
2022-03-31 18:05:33 +0530

    Create start.py

---
+++ plugins/start.py
@@ -0,0 +1,3 @@
+@Client.on_message(filters.command("start") & filters.incoming & ~filters.edited)
+async def alive(client, message):
+    await message.reply(f" Bot maybe a bit slow due to over load 🙁🙁..! please Don't spam..!")
