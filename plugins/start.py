8c9792d776d32b93c8ec2d53e9b044ded86df1ba
Masterolic <93469093+Masterolic@users.noreply.github.com>
2022-03-31 18:10:21 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,3 +1,4 @@
+from pyrogram import Client , filters 
 @Client.on_message(filters.command("start") & filters.incoming & ~filters.edited)
 async def alive(client, message):
     await message.reply(f" Bot maybe a bit slow due to over load 🙁🙁..! please Don't spam..!")
