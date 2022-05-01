5461d3f3c2017b8bd6f287708731346349ac6696
Masterolic <93469093+Masterolic@users.noreply.github.com>
2022-05-01 07:13:38 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -140,6 +140,8 @@ async def re_enable_chat(bot, message):
 @Client.on_message(filters.command('stats') & filters.incoming)
 async def get_ststs(bot, message):
     rju = await message.reply('⏳')
+    cpu = psutil.cpu_percent()
+    ram = psutil.virtual_memory().percent
     total_users = await db.total_users_count()
     totl_chats = await db.total_chat_count()
     files = await Media.count_documents()
